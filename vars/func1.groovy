cba3416bbf45d736315ab8ac38fcca9a96b7dc33
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-02 17:36:32 +0200

    asdsa

--- vars/func1.groovy
+++ vars/func1.groovy
@@ -13,10 +13,11 @@ def A funcHello(){
 	return new A(name:"moshe",age:"22");
 }
 
-def readConfigFile(String filePath){
-    def jsonSlurper = new JsonSlurper()
-    File f = new File(filePath)
-    return jsonSlurper.parseText(f.text)
+def void readConfigFile(String filePath){
+//    def jsonSlurper = new JsonSlurper()
+//    File f = new File(filePath)
+//    return jsonSlurper.parseText(f.text)
+    print(filePath)
 }
 
 
