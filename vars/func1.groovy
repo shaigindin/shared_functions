ccaf403f82448fdcc1e062195d75f2a809505d7e
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-02 14:52:45 +0200

    asds

--- vars/func1.groovy
+++ vars/func1.groovy
@@ -1,8 +1,11 @@
 import java.io.*
 import groovy.io.*
 
+class A{
+    String name;
+    int age;
+}
 def funcHello(){
-	println("hello friends from function!");
+	return A("moshe",22);
 }
 
-funcHello();
