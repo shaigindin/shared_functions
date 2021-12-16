3f0d47df37c05472f72a89af8e0f90b69ae8f46a
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-16 13:53:00 +0200

    asd

--- vars/techsomedUtility.groovy
+++ vars/techsomedUtility.groovy
@@ -10,16 +10,14 @@ import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
 import hudson.util.DescribableList;
 import jenkins.model.Jenkins;
-public createGlobalEnvironmentVariables(String key, String value){
- 
+
+
+def createGlobalEnvironmentVariables( key, value){ 
        Jenkins instance = Jenkins.getInstance();
- 
        DescribableList<NodeProperty<?>, NodePropertyDescriptor> globalNodeProperties = instance.getGlobalNodeProperties();
        List<EnvironmentVariablesNodeProperty> envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
- 
        EnvironmentVariablesNodeProperty newEnvVarsNodeProperty = null;
        EnvVars envVars = null;
- 
        if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
            newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
            globalNodeProperties.add(newEnvVarsNodeProperty);
