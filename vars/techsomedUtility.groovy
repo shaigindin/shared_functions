c4ef392057c69019ed0efddddd0cd80a57972d95
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-18 20:07:49 +0200

    asd

--- vars/techsomedUtility.groovy
+++ vars/techsomedUtility.groovy
@@ -113,12 +113,12 @@ def loadGitRepos(repos, branches){
         }
 }
 
-def tagRepo(paramMAp, repo_name){
+def tagRepo(paramMAp, repo_name, package_type){
 	// assert (fileExists(file: "${paramMAp.WORKSPACE}/pycommon/.venv") && fileExists(file: "${env.WORKSPACE}/pycommon/misc/version_util.py"))
 	withCredentials([gitUsernamePassword(credentialsId: '30bac85c-db0f-430c-9cd0-6bd25f2eb01a', gitToolName: 'git-tool')]) {
         bat "python ${paramMAp.WORKSPACE}/pycommon/misc/version_util.py --command 3 --repo_path ${paramMAp.WORKSPACE}/${repo_name} --type python --jenkins_id ${currentBuild.number}"
     }
-	// bat "python ${paramMAp.WORKSPACE}/${repo_name}/misc/version_util.py --command 3 --repo_path ${paramMAp.WORKSPACE}/${repo_name} --type python --jenkins_id ${currentBuild.number}"
+	// bat "python ${paramMAp.WORKSPACE}/${repo_name}/misc/version_util.py --command 3 --repo_path ${paramMAp.WORKSPACE}/${repo_name} --type ${package_type} --jenkins_id ${currentBuild.number}"
 }
 
 
