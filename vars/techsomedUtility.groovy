05bda6d500c4a98e2a7dc9b560336adc92580565
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-21 15:14:49 +0200

    sads

--- vars/techsomedUtility.groovy
+++ vars/techsomedUtility.groovy
@@ -55,9 +55,17 @@ def getFoldersFromPath(path){
 
 def filterLoadRepos(paramMAp, dependencies){
 	foldersList = getFoldersFromPath(paramMAp.WORKSPACE)
-	return (0..dependencies.size()-1).findAll(
-                       { fileExists(file: "${paramMAp.WORKSPACE}/${dependencies[it]}/Jenkins/ready_${paramMAp.BUILD_NUMBER}.txt")}).collect { dependencies[it] }
+	ls = []
+	for (i=0; i < dependencies.size(); i++){
+		if(fileExists(file: "${paramMAp.WORKSPACE}/${dependencies[it]}/Jenkins/ready_${paramMAp.BUILD_NUMBER}.txt"){
+			ls.add(dependencies[i)
+		}
+	}
+	return ls
 }
+//	return (0..dependencies.size()-1).findAll(
+//                      { )}).collect { dependencies[it] }
+
 
 @NonCPS
 def createFile(file_path){
