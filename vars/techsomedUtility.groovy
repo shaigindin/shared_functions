8af50caf696d0bc4feebac42950870d64fd147bb
shaigindin <49125116+shaigindin@users.noreply.github.com>
2021-12-21 11:44:50 +0200

    ads

--- vars/techsomedUtility.groovy
+++ vars/techsomedUtility.groovy
@@ -49,10 +49,12 @@ def readConfigFile(String jsonText){
 
 
 def cleanIt(paramMAp, dependencies){
-    println("${paramMAp.WORKSPACE}/${dependencies[0]}")
-	def cleanDependencies = (0..dependencies.size()-1).findAll(
-                        { fileExists(file: "${paramMAp.WORKSPACE}/${dependencies[it]}") }).collect { dependencies[it] }
-    println(cleanDependencies)
+    dlist = []
+	new File("${paramMAp.WORKSPACE}).eachDir {dlist << it.name }
+	println(dlist)
+	// def cleanDependencies = (0..dependencies.size()-1).findAll(
+    //                   { fileExists(file: "${paramMAp.WORKSPACE}/${dependencies[it]}") }).collect { dependencies[it] }
+    // println(cleanDependencies)
 }
 
 @NonCPS
